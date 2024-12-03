ab313f34772d206d4bdee90aeb89cd0501943332
Nara Díaz Viñolas <rdvdev2@gmail.com>
2024-12-03 22:51:11 +0100

    Remove docker machinery from flake, add .envrc

--- flake.nix
+++ flake.nix
@@ -12,41 +12,9 @@
         pkgs = nixpkgs.legacyPackages.${system};
         build_deps = with pkgs; [ zola ];
       in
-      rec {
+      {
         devShells.default = pkgs.mkShell {
           buildInputs = build_deps;
-        };
-
-        packages.default = packages.website;
-
-        packages.website = pkgs.stdenv.mkDerivation {
-          name = "naritanara-xyz";
-          src = ./.;
-          dontUnpack = true;
-          sourceRoot = ".";
-
-          buildInputs = build_deps;
-
-          buildPhase = ''
-            cd $src
-            zola build -o $out
-          '';
-        };
-
-        packages.website-server = pkgs.writeShellScriptBin
-          "naritanara-xyz-server" ''
-            ${pkgs.static-web-server}/bin/static-web-server --root ${packages.website} -p 80 $@
-          '';
-
-        packages.docker-image = pkgs.dockerTools.buildLayeredImage {
-          name = "naritanara-xyz";
-          tag = "latest";
-          config = {
-            Cmd = ["${packages.website-server}/bin/naritanara-xyz-server"];
-            ExposedPorts = {
-              "80/tcp" = {};
-            };
-          };
         };
       }
     );
